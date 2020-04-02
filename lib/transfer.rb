b95042721d40a9f53b8962fb56ec89da286b13fa
Jack Madden <jck13mad@gmail.com>
2020-04-02 22:17:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,6 +4,9 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
   
   def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
     
   
 end
