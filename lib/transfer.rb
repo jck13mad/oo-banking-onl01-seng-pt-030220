be438c68ecba00b7a291aac7ae124766219d6789
Jack Madden <jck13mad@gmail.com>
2020-04-02 22:17:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,8 @@
 require 'pry'
 class Transfer
   # your code here
-  attr_accessor :sender, :receiver, :status, 
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(name)
   
 end
