193573bf58dc68689b9ca0fdc5ced0af7a6db9d7
Jack Madden <jck13mad@gmail.com>
2020-04-02 22:20:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,8 @@ class Transfer
     if @sender.balance > amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
+      @status = "complete"
+    else
       
   end
   
