6868fc4eb9dd42b3459a835bb61f8bdee54a3e35
Jack Madden <jck13mad@gmail.com>
2020-04-02 22:48:09 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,9 @@
 class BankAccount
-  
   attr_accessor :balance, :status
   attr_reader :name
 
-  def initialize(name)
-    @name = name
+  def initialize(account_name)
+    @name = account_name
     @balance = 1000
     @status = "open"
   end
@@ -12,18 +11,16 @@ class BankAccount
   def deposit(money)
     @balance += money
   end
-  
+
   def display_balance
     "Your balance is $#{@balance}."
   end
-  
+
   def valid?
     @status == "open" && @balance > 0 ? true : false
   end
-  
+
   def close_account
     @status = "closed"
   end
-
-
 end
